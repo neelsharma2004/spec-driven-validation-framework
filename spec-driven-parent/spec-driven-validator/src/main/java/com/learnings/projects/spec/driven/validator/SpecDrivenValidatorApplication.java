c6b6647c6531740a18f33458f2ae472841a4f725
Neel <letsmovelove@gmail.com>
2026-05-23 12:42:16 +0530

    Initial checkin

---
+++ spec-driven-parent/spec-driven-validator/src/main/java/com/learnings/projects/spec/driven/validator/SpecDrivenValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.learnings.projects.spec.driven.validator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecDrivenValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecDrivenValidatorApplication.class, args);
+	}
+
+}
---
+++ spec-driven-parent/spec-driven-validator/src/test/java/com/learnings/projects/spec/driven/validator/SpecDrivenValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnings.projects.spec.driven.validator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpecDrivenValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
